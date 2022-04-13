fddaca954c2dffdb02ad86317cac745315f5d0c1
thecodeinfluencer <mbenjerminne@gmail.com>
2022-04-13 15:08:10 +0300

    added PORT

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import axios from 'axios';
-import cors from 'cors';
-import depositRoutes from './routes/depositRoutes.js';
-import callbackRoutes from './routes/callbackRoutes.js';
 import bodyParser from 'body-parser';
+import cors from 'cors';
+import express from 'express';
+import callbackRoutes from './routes/callbackRoutes.js';
+import depositRoutes from './routes/depositRoutes.js';
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(bodyParser.json({ extended: true }));
@@ -15,6 +15,6 @@ app.use(cors());
 app.use('/api/client/mpesa', depositRoutes);
 app.use('/api/client/mpesa-callback', callbackRoutes);
 
-app.listen(5000, () => {
-  console.log('listening on 5000');
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
 });
