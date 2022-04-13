fe04d613ff6d832ba62beab8f15a3645a601e3fc
thecodeinfluencer <mbenjerminne@gmail.com>
2022-04-13 15:17:07 +0300

    added description to logs

--- controllers/callbackController.js
+++ controllers/callbackController.js
@@ -1,4 +1,9 @@
 export const depositCallback = async (req, res, next) => {
-  console.log(req.body.Body.stkCallback);
-  console.log(req.body.Body.stkCallback.CallbackMetadata.Item);
+  console.log('-- Mpesa Callback --');
+  console.log('Req Body: ', req.body);
+  console.log('Req stkCallback: ', req.body?.Body?.stkCallback);
+  console.log(
+    'Req Metadata: ',
+    req.body?.Body?.stkCallback?.CallbackMetadata?.Item
+  );
 };
